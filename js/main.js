e33d20c771d1168af74f82f65faed2d6c0bbd912
letanthanh2004 <22130255@st.hcmuaf.edu.vn>
2025-10-26 13:58:02 +0700

    trang chi tiet sp_them load anh

--- js/main.js
+++ js/main.js
@@ -93,3 +93,20 @@
 
     lastScroll = currentScroll;
 });
+  const mainImage = document.getElementById('mainImage');
+  const thumbnails = document.querySelectorAll('.row_bottom_detail img');
+
+  thumbnails.forEach(img => {
+    img.addEventListener('click', () => {
+      // Đổi ảnh chính thành ảnh nhỏ vừa click
+      mainImage.src = img.src;
+
+      // Xóa class "active" ở các ảnh nhỏ khác
+      thumbnails.forEach(i => i.classList.remove('active'));
+
+      // Thêm class "active" vào ảnh vừa được chọn
+      img.classList.add('active');
+    });
+  });
+
+
