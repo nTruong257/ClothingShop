3db8cb038beba9ffe36feefc4f79b4974baab9e3
MinhPhucHCMUAF <22130215@st.hcmuaf.edu.vn>
2025-12-07 10:33:30 +0700

    fix: chinh sua loi nho co ban

--- src/main/java/com/clothingshop/styleera/JDBiConnector/PropertyDB.java
+++ src/main/java/com/clothingshop/styleera/JDBiConnector/PropertyDB.java
@@ -3,7 +3,7 @@ package com.clothingshop.styleera.JDBiConnector;
 public class PropertyDB {
     public final static String HOST = "localhost";
     public final static String PORT = "3306";
-    public final static String DBNAME = "demo";
+    public final static String DBNAME = "clothing_shop";
     public final static String USERNAME = "root";
     public final static String PASSWORD = "";
 }
--- src/main/java/com/clothingshop/styleera/dao/CartDao.java
+++ src/main/java/com/clothingshop/styleera/dao/CartDao.java
@@ -5,33 +5,33 @@ import com.clothingshop.styleera.model.CartItem;
 import org.jdbi.v3.core.Jdbi;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CartDao {
     public List<CartItem> getCartItems(int userId){
         Jdbi jdbi = JDBIConnector.getJdbi();
-        String sql = "SELECT \n" +
-                "                    ci.cart_item_id,\n" +
-                "                    p.product_id,\n" +
-                "                    v.variant_id,\n" +
-                "                    p.product_name,\n" +
-                "                    p.price,\n" +
-                "                    ci.quantity,\n" +
-                "                    (p.price * ci.quantity) AS total_price,\n" +
-                "                    img.path AS image_path\n" +
-                "                FROM CartItem ci\n" +
-                "                JOIN Variants v ON ci.variant_id = v.variant_id\n" +
-                "                JOIN Products p ON v.product_id = p.product_id\n" +
-                "                LEFT JOIN Images img \n" +
-                "                       ON img.product_id = p.product_id AND img.is_thumbnail = 1\n" +
-                "                WHERE ci.user_id = ?\n" +
-                "                ORDER BY ci.updated_at DESC\n";
+        List<CartItem> cart = jdbi.withHandle(handle -> {
+            String sql = "SELECT \n" +
+                    "                    ci.cart_item_id,\n" +
+                    "                    p.product_id,\n" +
+                    "                    v.variant_id,\n" +
+                    "                    p.product_name,\n" +
+                    "                    p.price,\n" +
+                    "                    ci.quantity,\n" +
+                    "                    (p.price * ci.quantity) AS total_price,\n" +
+                    "                    img.path AS image_path\n" +
+                    "                FROM CartItem ci\n" +
+                    "                JOIN Variants v ON ci.variant_id = v.variant_id\n" +
+                    "                JOIN Products p ON v.product_id = p.product_id\n" +
+                    "                LEFT JOIN Images img \n" +
+                    "                       ON img.product_id = p.product_id AND img.is_thumbnail = 1\n" +
+                    "                WHERE ci.user_id = ?\n" +
+                    "                ORDER BY ci.updated_at DESC\n";
+            return handle.createQuery(sql).bind(1, userId).mapToBean(CartItem.class).stream().collect(Collectors.toList());
+        });
 
-        return jdbi.withHandle(h ->
-                h.createQuery(sql)
-                        .bind(0, userId)
-                        .mapToBean(CartItem.class)
-                        .list()
-        );
+        return cart;
+
     }
 
 
