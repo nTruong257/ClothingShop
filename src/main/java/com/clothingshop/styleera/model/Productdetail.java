31b0503ab4653fee4d5555269d8b1ee16437ead0
nTruong257 <22130306@st.hcmuaf.edu.vn>
2026-01-06 00:24:01 +0700

    fix: minor fixes after pull

--- src/main/java/com/clothingshop/styleera/model/Productdetail.java
+++ src/main/java/com/clothingshop/styleera/model/Productdetail.java
@@ -1,4 +1,5 @@
 package com.clothingshop.styleera.model;
+
 import java.util.List;
 
 public class ProductDetail {
@@ -8,16 +9,46 @@ public class ProductDetail {
     private double price;
     private List<String> images; //
 
-    public ProductDetail() {}
+    public ProductDetail() {
+    }
 
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-    public String getDescription() { return description; }
-    public void setDescription(String description) { this.description = description; }
-    public double getPrice() { return price; }
-    public void setPrice(double price) { this.price = price; }
-    public List<String> getImages() { return images; }
-    public void setImages(List<String> images) { this.images = images; }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public List<String> getImages() {
+        return images;
+    }
+
+    public void setImages(List<String> images) {
+        this.images = images;
+    }
 }
